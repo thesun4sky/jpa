405cbada5670ad7ff3eb50750a74cfa4921342d9
teasun <teasun@wv-works.com>
2024-05-29 01:54:38 +0900

    fix(db) : DB 정보를 H2 데이터베이스로 변경

--- src/test/java/me/whitebear/jpa/jdbc/JDBCTest.java
+++ src/test/java/me/whitebear/jpa/jdbc/JDBCTest.java
@@ -4,82 +4,70 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import me.whitebear.jpa.jdbc.dao.AccountDAO;
-import me.whitebear.jpa.jdbc.vo.AccountVO;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import me.whitebear.jpa.jdbc.dao.AccountDAO;
+import me.whitebear.jpa.jdbc.vo.AccountVO;
+
 public class JDBCTest {
+	String url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE";
+	String username = "sa";
+	String password = null;
 
+	@BeforeEach
+	void setUp() {
 
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+			String createSql = "CREATE TABLE ACCOUNT (id SERIAL PRIMARY KEY, username varchar(255), password varchar(255))";
+			try (PreparedStatement statement = connection.prepareStatement(createSql)) {
+				statement.execute();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-  @Test
-  @DisplayName("JDBC 테이블 생성 실습")
-  void jdbcTest() throws SQLException {
-    // docker exec -i -t postgres_boot bash
-    // su - postgres
-    // psql --username teasun --dbname messenger
-    // \list (데이터 베이스 조회)
-    // \dt (테이블 조회)
+	}
 
-    String url = "jdbc:postgresql://localhost:5432/messenger";
-    String username = "teasun";
-    String password = "pass";
+	@Test
+	@DisplayName("JDBC 테이블 생성/추가/조회 실습")
+	void jdbcTest() throws SQLException {
+		// when
+		// when
+		try (Connection connection = DriverManager.getConnection(url, username, password)) {
+			System.out.println("Connection created: " + connection);
 
-    // when
-    try(Connection connection = DriverManager.getConnection(url, username, password)) {
-      String createSql = "CREATE TABLE ACCOUNT (id SERIAL PRIMARY KEY, username varchar(255), password varchar(255))";
-      try(PreparedStatement statement = connection.prepareStatement(createSql)) {
-        statement.execute();
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+			String insertSql = "INSERT INTO ACCOUNT (id, username, password) VALUES ((SELECT coalesce(MAX(ID), 0) + 1 FROM ACCOUNT A), 'user1', 'pass1')";
+			try (PreparedStatement statement = connection.prepareStatement(insertSql)) {
+				statement.execute();
+			}
 
-  }
+			// then
+			String selectSql = "SELECT * FROM ACCOUNT";
+			try (PreparedStatement statement = connection.prepareStatement(selectSql)) {
+				var rs = statement.executeQuery();
+				while (rs.next()) {
+					System.out.printf("%d, %s, %s", rs.getInt("id"), rs.getString("username"),
+						rs.getString("password"));
+				}
+			}
+		}
+	}
 
+	@Test
+	@DisplayName("JDBC DAO 삽입/조회 실습")
+	void jdbcDAOInsertSelectTest() throws SQLException {
+		// given
+		AccountDAO accountDAO = new AccountDAO();
 
-  @Test
-  @DisplayName("JDBC 삽입/조회 실습")
-  void jdbcInsertSelectTest() throws SQLException {
-    // given
-    String url = "jdbc:postgresql://localhost:5432/messenger";
-    String username = "teasun";
-    String password = "pass";
+		// when
+		var id = accountDAO.insertAccount(new AccountVO("new user", "new password"));
 
-    // when
-    try (Connection connection = DriverManager.getConnection(url, username, password)) {
-      System.out.println("Connection created: " + connection);
-
-      String insertSql = "INSERT INTO ACCOUNT (id, username, password) VALUES ((SELECT coalesce(MAX(ID), 0) + 1 FROM ACCOUNT A), 'user1', 'pass1')";
-      try (PreparedStatement statement = connection.prepareStatement(insertSql)) {
-        statement.execute();
-      }
-
-      // then
-      String selectSql = "SELECT * FROM ACCOUNT";
-      try (PreparedStatement statement = connection.prepareStatement(selectSql)) {
-        var rs = statement.executeQuery();
-        while (rs.next()) {
-          System.out.printf("%d, %s, %s", rs.getInt("id"), rs.getString("username"),
-              rs.getString("password"));
-        }
-      }
-    }
-  }
-
-  @Test
-  @DisplayName("JDBC DAO 삽입/조회 실습")
-  void jdbcDAOInsertSelectTest() throws SQLException {
-    // given
-    AccountDAO accountDAO = new AccountDAO();
-
-    // when
-    var id = accountDAO.insertAccount(new AccountVO("new user", "new password"));
-
-    // then
-    var account = accountDAO.selectAccount(id);
-    assert account.getUsername().equals("new user");
-  }
+		// then
+		var account = accountDAO.selectAccount(id);
+		assert account.getUsername().equals("new user");
+	}
 
 }
--- src/test/java/me/whitebear/jpa/jdbc/dao/AccountDAO.java
+++ src/test/java/me/whitebear/jpa/jdbc/dao/AccountDAO.java
@@ -5,71 +5,72 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import me.whitebear.jpa.jdbc.vo.AccountVO;
 
 public class AccountDAO {
 
-    // JDBC 관련 변수
-  private Connection conn = null;
+	// JDBC 관련 변수
+	private Connection conn = null;
 
-  private PreparedStatement stmt = null;
+	private PreparedStatement stmt = null;
 
-  private ResultSet rs = null;
+	private ResultSet rs = null;
 
-  private static final String url = "jdbc:postgresql://localhost:5432/messenger";
-  private static final String username = "teasun";
-  private static final String password = "pass";
+	private static final String url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE";
+	private static final String username = "sa";
+	private static final String password = null;
 
-  // SQL 쿼리
-  private final String ACCOUNT_INSERT = "INSERT INTO account(ID, USERNAME, PASSWORD) "
-      + "VALUES((SELECT coalesce(MAX(ID), 0) + 1 FROM ACCOUNT A), ?, ?)";
-  private final String ACCOUNT_SELECT = "SELECT * FROM account WHERE ID = ?";
+	// SQL 쿼리
+	private final String ACCOUNT_INSERT = "INSERT INTO account(ID, USERNAME, PASSWORD) "
+		+ "VALUES((SELECT coalesce(MAX(ID), 0) + 1 FROM ACCOUNT A), ?, ?)";
+	private final String ACCOUNT_SELECT = "SELECT * FROM account WHERE ID = ?";
 
-  // CRUD 기능 메소드
-  public Integer insertAccount(AccountVO vo) {
-    var id = -1;
-    try {
-      String[] returnId = {"id"};
-      conn = DriverManager.getConnection(url, username, password);
-      stmt = conn.prepareStatement(ACCOUNT_INSERT, returnId);
-      stmt.setString(1, vo.getUsername());
-      stmt.setString(2, vo.getPassword());
-      stmt.executeUpdate();
+	// CRUD 기능 메소드
+	public Integer insertAccount(AccountVO vo) {
+		var id = -1;
+		try {
+			String[] returnId = {"id"};
+			conn = DriverManager.getConnection(url, username, password);
+			stmt = conn.prepareStatement(ACCOUNT_INSERT, returnId);
+			stmt.setString(1, vo.getUsername());
+			stmt.setString(2, vo.getPassword());
+			stmt.executeUpdate();
 
-      try (ResultSet rs = stmt.getGeneratedKeys()) {
-        if (rs.next()) {
-          id = rs.getInt(1);
-        }
-      }
+			try (ResultSet rs = stmt.getGeneratedKeys()) {
+				if (rs.next()) {
+					id = rs.getInt(1);
+				}
+			}
 
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-    return id;
-  }
+		return id;
+	}
 
-  public AccountVO selectAccount(Integer id) {
+	public AccountVO selectAccount(Integer id) {
 
-    AccountVO vo = null;
+		AccountVO vo = null;
 
-    try {
-      conn = DriverManager.getConnection(url, username, password);
-      stmt = conn.prepareStatement(ACCOUNT_SELECT);
-      stmt.setInt(1, id);
-      var rs = stmt.executeQuery();
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+			stmt = conn.prepareStatement(ACCOUNT_SELECT);
+			stmt.setInt(1, id);
+			var rs = stmt.executeQuery();
 
-      if (rs.next()) {
-        vo = new AccountVO();
-        vo.setId(rs.getInt("ID"));
-        vo.setUsername(rs.getString("USERNAME"));
-        vo.setPassword(rs.getString("PASSWORD"));
-      }
+			if (rs.next()) {
+				vo = new AccountVO();
+				vo.setId(rs.getInt("ID"));
+				vo.setUsername(rs.getString("USERNAME"));
+				vo.setPassword(rs.getString("PASSWORD"));
+			}
 
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-    return vo;
-  }
+		return vo;
+	}
 }
